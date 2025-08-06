2b9665d5f33d5333e14d32260917c726634e2c23
Letícia de Abreu <abreuhleticia@email.com>
2025-08-06 10:37:07 -0300

    atualizado

---
+++ ProgramaPoupanca.java
@@ -0,0 +1,11 @@
+impot java.util.Scanner;
+
+public class CalculoPoupanca{
+    public static void main(String[]args){
+        Scanner scanner = new Scanner (Systen.in);
+
+        int idade;
+
+        
+    }
+}
