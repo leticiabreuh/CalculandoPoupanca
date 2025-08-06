a00905cda9da982d98f4a66b43c6d75ec2101856
Letícia de Abreu <abreuhleticia@email.com>
2025-08-06 11:00:42 -0300

    atualizado

--- ProgramaPoupanca.java
+++ ProgramaPoupanca.java
@@ -4,8 +4,27 @@ public class CalculoPoupanca{
     public static void main(String[]args){
         Scanner scanner = new Scanner (Systen.in);
 
-        int idade;
+        int anos;
+        double juros;
+        double depositoMensal;
+        int qtdAnos;
+        double total;
 
-        
+        anos = scanner.nextInt();
+        jurosMes = scanner.nextDouble();
+        depositoMensal = scanner.nextDouble();
+
+        juros = 0.05 /100;
+        qtdMeses = anos * 12;
+        total = (depositoMensal * (Math.pow(1+juros, qtdMeses) - 1)) / juros;
+
+       System.out.println("Total poupado R$: "+total);
+
+
+
+
+
+
+
     }
 }
